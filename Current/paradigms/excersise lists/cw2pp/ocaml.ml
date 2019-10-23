650a6bc78bd9fb05d268ce3db7769c6e660d5155
Dawid <dawid.marszalek99@gmail.com>
2019-10-23 12:25:01 +0200

    update 23.10.19
    some updates, done in paradigms classes Kappa

--- Current/paradigms/excersise lists/cw2pp/ocaml.ml
+++ Current/paradigms/excersise lists/cw2pp/ocaml.ml
@@ -24,31 +24,36 @@ let fibTail x =
 (*zadanie 3*)
 
 (* let findCubeRoot (x, sigma) =     *)
-(* 	let rec findCubeRootTail(acc, ) *)
 
 (*zadanie 4*)
+(*przykłady takich wzorców*)
 
-let patternMatch1(xs, x) =
-	match xs with
-	| [_;_;x;_;_] -> true
-	| _ -> false
-	
-let patternMatch2 (xs, x) =
-	match xs with
-	| [(_,_);(x,_)] -> true
-	| _ -> false
-
-
+let x = 0;;
 let list1 = [-2;-1;0;1;2]
 let list2 = [(1,2);(0,1)]
 
-patternMatch1 (list1, 20)
-patternMatch2 (list2, 1)
+let [_;_;x;_;_] = [-2;-1;0;1;2]
+let _::_::x::_::_::[] = list1
+let [(_,_);(x,_)] = list2
 
-(*kod z zad4 nie działa, za każdym razem daje true*)
+let _::(-1)::x::_ = list1
+
+
+(* przykłady działających wzorców: *)
+(* a: *)
+[_;_;x;_;_]
+_::_::x::_::_::[]
+(* b: *)
+[(_,_);(x,_)]
+_::[(x,_)]
+
+
+let list3 = 1::2::3::4::[]
+
 
 (*zadanie 5*)
 
+(*xs to fragment listy, ys to lista*)
 let rec initSegment(xs, ys) =
 	match (xs, ys) with
 	| ([], _) -> true
@@ -68,6 +73,17 @@ initSegment(segment3, newlist)
 
 (*zadanie 6*)
 
+
+let rec replaceNth (xs, index, x) =
+	match (xs, index) with
+	| ([],_) -> []
+	| (h::t, 0) -> x::t
+	| (h::t, index) -> h::replaceNth(t, index-1, x);;
+
+replaceNth([1;2;3;4;5;5;6;7;9], 4, 5);;
+
+(*dla wartości index większej niż 2 *)
+(*program wyrzuca Exception: Match_failure ("//toplevel//", 10, 1).*)
 
 
 
--- Current/paradigms/excersise lists/cw2pp/scala.scala
+++ Current/paradigms/excersise lists/cw2pp/scala.scala
@@ -28,9 +28,26 @@ val b = fibTail(12)
 
 //zadanie 4
 
+val x = 0
+
+def list1 = List(-2,-1,0,1,2)
+def list2 = List((1, 2), (0, 1))
+
+//pasujące wzorce:
+//a:
+
+List(_,_,x,_,_)
+_::_::x::_::_
+
+//b:
+
+List((_,_),(x,_))
+_::List((x,_))
 
 //zadanie 5
 
+
+//xs to segment listy, ys to lista
 def initSegment [A](xs:List[A], ys:List[A]):Boolean =
   (xs, ys) match{
   case (Nil, _) => true
@@ -39,9 +56,20 @@ def initSegment [A](xs:List[A], ys:List[A]):Boolean =
             else false
 }
 
+initSegment(List(1,2,3), List(1,2,3,4))
 
-  
-  
+//zadanie 6
+
+def replaceNth [A](xs:List[A], index:Int, x:A):List[A] =
+  (xs, index) match {
+  case (Nil,_) => Nil
+  case (h::t, 0) => x::t
+  case (h::t, _) => h::replaceNth(t, index-1, x)
+}
+ 
+replaceNth(List(1,2,3,4,6), 3, 2)
+
+
   
   
   
